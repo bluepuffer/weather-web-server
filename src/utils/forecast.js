e3b43b2cee2986c207bba68194c13bf1dbf90437
jwang97133 <gjwang.work@gmail.com>
2023-02-02 09:24:20 -0500

    Make it work 3

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.meteomatics.com/2023-02-02T00:00:00Z/t_2m:C,relative_humidity_2m:p/35.771626,-78.780665/json'
 
     request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
+        // if (error) {
+        //     callback('Unable to connect to weather service!', undefined)
+        // } else if (body.error) {
+        //     callback('Unable to find location', undefined)
+        // } else {
+        //     callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        // }
+        callback(undefined, ' It is currently gppd degress out. There is a 0% chance of rain.')
     })
 }
 
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,21 +1,21 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
-
-    // request({ url, json: true }, (error, { body }) => {
-    //     if (error) {
-    //         callback('Unable to connect to location services!', undefined)
-    //     } else if (body.features.length === 0) {
-    //         callback('Unable to find location. Try another search.', undefined)
-    //     } else {
-    //         callback(undefined, {
-    //             latitude: body.features[0].center[1],
-    //             longitude: body.features[0].center[0],
-    //             location: body.features[0].place_name
-    //         })
-    //     }
-    // })
+    const url = 'http://api.positionstack.com/v1/forward?access_key=ca85e1fd772c159054b864badae49cd4&&query=cary nc'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.data.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            console.log('===== ' + body.data[0].latitude)
+            callback(undefined, {
+                latitude: body.data[0].latitude,
+                longitude: body.data[0].longitude,
+                location: body.data[0].locality
+            })
+        }
+    })
 }
 
 module.exports = geocode
